2e13f7a2b06c3296904317d0db274efd45e7e105
Eric N <ericntd@gmail.com>
2023-02-18 16:23:12 -0500

    Used countries names to improve UX

--- app/src/main/java/com/example/rvlistadapter/MainActivity.kt
+++ app/src/main/java/com/example/rvlistadapter/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
+import kotlin.math.min
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -17,19 +18,36 @@ class MainActivity : AppCompatActivity() {
     private val adapter by lazy {
         ClassicRvAdapter()
     }
-    private val numItems = 100
+
+    /**
+     * Must be smaller than size of [DB.countries]
+     */
+    private val numItems = 3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<RecyclerView>(R.id.list).adapter = adapter
 
-        val data = (0..numItems).map {
-            DummyData(it, UUID.randomUUID().toString().replace("-", " "))
-        }
+        /*
+        Initialize list view with numItems random subset of countries
+         */
+        val randomStart = Random.nextInt(DB.countries.size)
+        val end = min(randomStart + numItems, DB.countries.size-1)
+        val data = DB.countries.mapIndexed { index: Int, s: String ->
+            DummyData(id = index, content = s)
+        }.subList(randomStart, end)
         adapter.updateAll(data)
+
+        /*
+        Add random UUID string, there should be no chance of duplication
+         */
         findViewById<View>(R.id.add).setOnClickListener {
-            val randomIndex = Random.nextInt(adapter.itemCount)
+            val randomIndex = if (adapter.itemCount>0) {
+                Random.nextInt(adapter.itemCount)
+            } else {
+                0
+            }
             adapter.add(
                 DummyData(
                     id = numItems + Random.nextInt(numItems),
