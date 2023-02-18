53010129a4f0e2bde42f8d58bc567ca3b5accd97
Eric N <ericntd@gmail.com>
2023-02-18 16:37:37 -0500

    Shuffle country ranking

--- app/src/main/java/com/example/rvlistadapter/MainActivity.kt
+++ app/src/main/java/com/example/rvlistadapter/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     /**
      * Must be smaller than size of [DB.countries]
      */
-    private val numItems = 3
+    private val numItems = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,17 +33,17 @@ class MainActivity : AppCompatActivity() {
         Initialize list view with numItems random subset of countries
          */
         val randomStart = Random.nextInt(DB.countries.size)
-        val end = min(randomStart + numItems, DB.countries.size-1)
+        val end = min(randomStart + numItems, DB.countries.size - 1)
         val data = DB.countries.mapIndexed { index: Int, s: String ->
             DummyData(id = index, content = s)
-        }.subList(randomStart, end)
+        }.shuffled().subList(randomStart, end)
         adapter.updateAll(data)
 
         /*
         Add random UUID string, there should be no chance of duplication
          */
         findViewById<View>(R.id.add).setOnClickListener {
-            val randomIndex = if (adapter.itemCount>0) {
+            val randomIndex = if (adapter.itemCount > 0) {
                 Random.nextInt(adapter.itemCount)
             } else {
                 0
